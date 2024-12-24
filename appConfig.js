db5733ff4094ff45aae2cb9941731496734f329d
engma12 <barangunikyess@gmail.com>
2024-12-25 00:30:07 +0700

    Add files via upload

---
+++ appConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: "outfitidea",
+  tagline: "From Kitchen to Table – Recipes That Bring Joy!",
+  description: "Explore a treasure trove of recipes that cater to every craving and skill level. From hearty dinners to decadent desserts, our easy-to-follow guides turn cooking into a delightful experience. Perfect for home cooks, foodies, and anyone looking to make every meal memorable!",
+  keywords: "recipes, easy cooking, quick meals, healthy dishes, family recipes, comfort food, baking ideas, gourmet meals, vegetarian options, meal prep, step-by-step recipes, international cuisine, dinner ideas, holiday recipes, homemade cooking.,recipes, cooking inspiration, easy meals, healthy recipes, quick dinners, baking, comfort food, international flavors, vegetarian dishes, meal prep, holiday cooking, gourmet ideas, family-friendly meals, one-pot recipes, homemade food,recipes, easy recipes, quick meals, dinner ideas, healthy cooking, dessert recipes, family meals, international cuisine, vegetarian recipes, meal prep ideas, home cooking tips, gourmet recipes, food blog, kitchen hacks, baking recipes, Summer salads, Fall soups, Winter stews, Spring vegetables, Farm-to-table recipes, Cheap and easy recipes, Leftover makeovers, Pantry meals, Affordable dinners, Frugal cooking tips, Freezer-friendly recipes, Batch cooking, Weekly meal plans, Lunch prep ideas, Quick reheatable meals, Appetizers and snacks, Party drinks, Crowd-pleasing dishes, Festive desserts, Potluck recipes",
+  author: "outfitidea",
+  favicon: "",
+  imageCount: 10,
+  removeBadWords: "porn,nude,sex",
+};
